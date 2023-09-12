b0c1eece43e6d92ab5b93b1851469b9f9c191da9
cesar velasco <caacesar.ariza@gmail.com>
2023-09-11 23:05:51 -0500

    primer commit

---
+++ src/IdentityServer/Config.cs
@@ -0,0 +1,22 @@
+﻿using Duende.IdentityServer.Models;
+
+namespace IdentityServer;
+
+public static class Config
+{
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new IdentityResource[]
+        { 
+            new IdentityResources.OpenId()
+        };
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+        new List<ApiScope>
+            {
+            new ApiScope(name:"api1",displayName:"MyApi")
+        };
+
+    public static IEnumerable<Client> Clients =>
+        new Client[] 
+            { };
+}
